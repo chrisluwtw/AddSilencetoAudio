66244804ff502f5b8cf3aace519d57a26c1e9a54
test <test>
2023-04-05 09:51:35 -0700

    "added on screen message for electron"

--- main.js
+++ main.js
@@ -1,31 +1,59 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
+    width: 1200,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
     },
+    autoHideMenuBar: true, // 隐藏菜单栏
   });
-  win.webContents.openDevTools();
 
-  win.loadFile('index.html');
+ // win.webContents.openDevTools();
+
+  win.loadFile("index.html");
+  mainWindow.setMenu(null); // 完全移除菜单栏
+
+  // Log messages from renderer process
+  ipcMain.on("log", (event, message) => {
+    console.log(message);
+    mainWindow.webContents.send("log", message);
+  });
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
+
+// Listen for console.log messages from the renderer process
+ipcMain.on("console-log", (event, message) => {
+  //console.log("From renderer:", message);
+    // Convert the UTF-8 encoded message back to the original string
+    const originalMessage = Buffer.from(message, 'utf8').toString();
+
+    // Process the message
+    console.log('Received message:', originalMessage);
+
+});
+
+// ipcMain.on('send-main-window', (event, mainWindow) => {
+//   global.mainWindow = mainWindow;
+// });
+
+// ipcMain.on('update-progress', (event, progress) => {
+//   mainWindow.webContents.send('update-progress', progress);
+// });
--- render.js
+++ render.js
@@ -3,9 +3,31 @@ const fs = require("fs");
 const ffmpegPath = require("ffmpeg-static");
 const ffmpeg = require("fluent-ffmpeg");
 ffmpeg.setFfmpegPath(ffmpegPath);
+const {ipcRenderer} = require('electron');
+
+
+function logToConsole(message) {
+      // Convert message to a UTF-8 encoded string
+      const utf8Message = Buffer.from(message).toString('utf8');
+
+      // Send message to the main process
+     ipcRenderer.send('console-log', utf8Message);
+
+
+
+    // Display message in the HTML element
+    const consoleOutput = document.getElementById('console-output');
+    const logLine = document.createElement('p');
+    logLine.textContent = message;
+    consoleOutput.appendChild(logLine);
+}
+
+// Use logToConsole instead of console.log
+logToConsole('填好需要处理的音频文件信息!');
 
 
 const addSilence = (input, output, silenceDuration) => {
+
   return new Promise((resolve, reject) => {
     ffmpeg()
       .input(input)
@@ -17,18 +39,27 @@ const addSilence = (input, output, silenceDuration) => {
       ])
       .on("start", () => {
         console.log("开始处理音频文件...");
+        logToConsole("开始处理音频文件...");
+
+  
       })
       .on("progress", (progress) => {
         if (progress && progress.percent) {
           console.log(`处理进度: ${progress.percent.toFixed(2)}%`);
+          logToConsole(`处理进度: ${progress.percent.toFixed(2)}%`);
+
         }
       })
       .on("end", () => {
         console.log("处理完成");
+        logToConsole("处理完成...");
+
+        
         resolve();
       })
       .on("error", (err) => {
         console.error("处理错误: ", err);
+        logToConsole("处理错误 "+err);
         reject(err);
       })
       .save(output);
