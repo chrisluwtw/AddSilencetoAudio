d7e11b0b0f8cbeecd6dc4f36b8f4c0c2db2ad9ab
test <test>
2023-04-05 22:27:18 -0700

    uppdate package.json & electron  build exe working

--- render.js
+++ render.js
@@ -1,8 +1,21 @@
 // renderer.js
 const fs = require("fs");
-const ffmpegPath = require("ffmpeg-static");
+// const ffmpegPath = require("ffmpeg-static");
 const ffmpeg = require("fluent-ffmpeg");
-ffmpeg.setFfmpegPath(ffmpegPath);
+
+
+
+//Get the paths to the packaged versions of the binaries we want to use
+const ffmpegPath = require('ffmpeg-static').replace(
+  'app.asar',
+  'app.asar.unpacked'
+);
+
+
+//tell the ffmpeg package where it can find the needed binaries.
+//ffmpeg.setFfmpegPath(ffmpegPath);
+
+
 const {ipcRenderer} = require('electron');
 
 
